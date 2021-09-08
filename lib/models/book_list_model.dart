7199a3764153f1fa91f7188043648f0f1e972ff1
zoniha <awaawapurin@gmail.com>
2021-09-08 18:21:08 +0900

    ✨ feat: Update

--- lib/models/book_list_model.dart
+++ lib/models/book_list_model.dart
@@ -3,10 +3,11 @@ import 'package:firebase_sample/domain/book.dart';
 import 'package:flutter/material.dart';
 
 class BookListModel extends ChangeNotifier {
+  final _userCollection = FirebaseFirestore.instance.collection('books');
   List<Book>? books;
 
   void fetchBookList() async {
-    final QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('books').get();
+    final QuerySnapshot snapshot = await _userCollection.get();
 
     final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
       Map<String, dynamic> data = document.data() as Map<String, dynamic>;
